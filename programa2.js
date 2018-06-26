9923b8812fab0b9e81856edc9605a37a6e97509f
cbayonao <33583592+cbayonao@users.noreply.github.com>
2018-06-26 18:28:45 -0500

    Add files via upload

---
+++ programa2.js
@@ -0,0 +1,8 @@
+var texto = document.getElementById("actualizacion")
+var boton = document.getElementById("botoncito")
+boton.addEventListener("click", actualizacionPorClick);
+
+function actualizacionPorClick()
+{
+  var xxx = (texto.value);
+}
